757639be722bf5b1b0cb1cb84aec002c2c944bd1
Harsh Vardhan Tiwari <mr.glassman@Harshs-MacBook-Air.local>
2019-01-20 22:45:49 +0530

    organised post_status & timeline

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
 var Twitter = require('twitter');
 var cors = require('cors');
-var express = require('express'); // Express web server framework
-var request = require('request'); // "Request" library
+var express = require('express'); 
+var request = require('request'); 
 var cookieParser = require('cookie-parser');
-var imp = require('./test.js');
+var test = require('./test.js');
 var app = express();
 
 app.use(express.static(__dirname + '/public'))
@@ -13,110 +13,78 @@ app.use(express.static(__dirname + '/public'))
 app.listen(8000);
 console.log("listening to port 8000");
  
-var CONSUMER_KEY = '9YfiigzkfEONfI79hWrQHIDdY';
-var CONSUMER_SECRET = 'AC5wYgbc80Qu07ZLWwZKvQTAq1286FSvC7fCybbntBQF9hlhAC';
+var CONSUMER_KEY = '*****';
+var CONSUMER_SECRET = '******';
 
-var qs = require('querystring')
-  , oauth =
-    { callback: 'http://localhost:8000/callback'
-    , consumer_key: CONSUMER_KEY
-    , consumer_secret: CONSUMER_SECRET
-    , signature_method: 'HMAC-SHA1'
-    , version: '1.0'
-    , nonce: "4572616e48616d6d65724c61686176",
-    transport_method : 'query'
-    
-    }
-  , url = 'https://api.twitter.com/oauth/request_token'
-  ;
-
-app.use('/login', function(req,res,next){
-  
-
-
-  
-request.post({url:url, oauth:oauth}, function (e, r, body) {
-  
-  var req_data = qs.parse(body)
-
-  var uri = 'https://api.twitter.com/oauth/authenticate'
-    + '?' + qs.stringify({oauth_token: req_data.oauth_token})
-  // redirect the user to the authorize uri
-
- res.redirect(302,uri);
-  
-
-
-app.use('/callback', function(req,res,next){
-
- var alpha = req.query.oauth_verifier;
-  var auth_data = qs.parse(body)
-  , oauth =
-    { consumer_key: CONSUMER_KEY
-    , consumer_secret: CONSUMER_SECRET
-    , token: auth_data.oauth_token
-    , token_secret: req_data.oauth_token_secret
-    , verifier: alpha
-    }
-  , url = 'https://api.twitter.com/oauth/access_token'
-  ;
-  
-
-  request.post({url:url, oauth:oauth}, function (e, r, body) {
-    // ready to make signed requests on behalf of the user
-    
-        var fw =  qs.parse(body);
-        var token = fw.oauth_token;
-        var token_secret = fw.oauth_token_secret;
- 
-        var client = new Twitter({
-          consumer_key: CONSUMER_KEY,
-          consumer_secret: CONSUMER_SECRET,
-          access_token_key: token,
-          access_token_secret: token_secret
-    }); 
-    module.exports = client;
-    
-    var param;
-
-    
- var neil = function(param){    
-       client.get('statuses/home_timeline',   function(error, tweet, response) {
-         if(error) throw error;
-         
-         var fs = require('fs');
-         var fuck =  JSON.stringify(tweet);
-           
-       fs.appendFile(param+'.json', fuck, function (err) {
-         if (err) throw err;
-         console.log('Updated!');
-         module.exports = JSON.stringify(client);
-         res.send(fuck.length);
-       });
-       
-       });
-     }
-  
-  neil('lexluth21817810');
-  
-    var raby = function(count){    
-      client.post('statuses/update', {status: "sdsfgsgjegejg"},  function(error, tweet, response) {
-        if(error) throw error;
-       // Tweet body.
-        count++;
+    var qs = require('querystring')
+      , oauth =
+        { callback: 'http://localhost:8000/callback'
+        , consumer_key: CONSUMER_KEY
+        , consumer_secret: CONSUMER_SECRET
+        , signature_method: 'HMAC-SHA1'
+        , version: '1.0'
+        , nonce: "4572616e48616d6d65724c61686176",
+        transport_method : 'query'
         
-        if(count<100){
-          raby(count);
         }
-        else{
-          return;
-        }
-      });
-    }
-    
+      , url = 'https://api.twitter.com/oauth/request_token'
+      ;
 
+    app.use('/login', function(req,res,next){
+        request.post({url:url, oauth:oauth}, function (e, r, body) {
+          var req_data = qs.parse(body)
 
-      })
-    })
-    })
-  })
+          var uri = 'https://api.twitter.com/oauth/authenticate'
+            + '?' + qs.stringify({oauth_token: req_data.oauth_token})
+          // redirect the user to the authorize uri
+          res.redirect(302,uri);
+
+          app.use('/callback', function(req,res,next){
+                  var alpha = req.query.oauth_verifier;
+                    var auth_data = qs.parse(body)
+                    , oauth =
+                      { consumer_key: CONSUMER_KEY
+                      , consumer_secret: CONSUMER_SECRET
+                      , token: auth_data.oauth_token
+                      , token_secret: req_data.oauth_token_secret
+                      , verifier: alpha
+                      }
+                    , url = 'https://api.twitter.com/oauth/access_token'
+                    ;
+                    
+                    request.post({url:url, oauth:oauth}, function (e, r, body) {
+                       
+                          var fw =  qs.parse(body);
+                          var token = fw.oauth_token;
+                          var token_secret = fw.oauth_token_secret;
+                  
+                          var client = new Twitter({
+                            consumer_key: CONSUMER_KEY,
+                            consumer_secret: CONSUMER_SECRET,
+                            access_token_key: token,
+                            access_token_secret: token_secret
+                           }); 
+                       
+                      var user;
+
+                      var timeline = function(user){    
+                        client.get('statuses/home_timeline',   function(error, tweet, response) {
+                          if(error) throw error;
+                          test(user,tweet);
+                        });
+                      }
+                    
+                    timeline('user');
+                    
+                      var post_status = function(user){    
+                        client.post('statuses/update', {status: "change_text"},  function(error, tweet, response) {
+                          if(error) throw error;
+                          test(user,tweet);
+                        });
+                      }
+                      post_status('user');
+
+                    })
+            })
+            })
+          })
--- test.js
+++ test.js
@@ -7,9 +7,14 @@ var cors = require('cors');
 
 var index = require('./index.js');
 
+var file = function(user,tweet){
+    var fs = require('fs');
+        var str =  JSON.stringify(tweet);        
+        fs.appendFile(user+'.json', str, function (err) {
+        if (err) throw err;
+        console.log('Updated!'); 
+        });                        
+    }
 
-    
 
-
-
-
+module.exports = file;
